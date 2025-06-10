21b6966576cec7f9b359187ae3094c8528db383b
Shubham V Singalkar <ssingalkar@avaya.com>
2025-06-10 15:10:37 +0530

    Updated the Entity class

--- src/main/java/com/shubham/sbapp/entity/Employee.java
+++ src/main/java/com/shubham/sbapp/entity/Employee.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-
 //@NoArgsConstructor
 @Entity
 public class Employee {
@@ -18,26 +17,7 @@ public class Employee {
 	private double esalary;
 	private String place;
 
-	/*
-	 * public Employee() { // TODO Auto-generated constructor stub }
-	 * 
-	 * public Employee(Integer eid, String ename, double esalary, String place) {
-	 * super(); this.eid = eid; this.ename = ename; this.esalary = esalary;
-	 * this.place = place; }
-	 * 
-	 * public Integer getEid() { return eid; } public void setEid(Integer eid) {
-	 * this.eid = eid; } public String getEname() { return ename; } public String
-	 * getPlace() { return place; }
-	 * 
-	 * public void setPlace(String place) { this.place = place; }
-	 * 
-	 * public void setEname(String ename) { this.ename = ename; } public double
-	 * getEsalary() { return esalary; } public void setEsalary(double esalary) {
-	 * this.esalary = esalary; }
-	 * 
-	 * @Override public String toString() { return "Employee [eid=" + eid +
-	 * ", ename=" + ename + ", esalary=" + esalary + ", place=" + place + "]"; }
-	 */
+
 
 
 
